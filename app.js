a0016c356d3310b80638f6f2a40fbc4d7c812c1c
AfterWin <CJ_Zheng1023@hotmail.com>
2018-01-11 18:09:59 +0800

    展示在线用户列表完成

--- app.js
+++ app.js
@@ -6,8 +6,26 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/views/index.html')
 })
-io.on('connetion', function(socket){
 
+/**
+ * 存储在线用户
+ * @type {Array}
+ */
+var users = [];
+io.on('connection', function(socket){
+    socket.on('addUser', function(user){
+        users.push(user);
+        socket.user = user;
+        socket.emit('queryUser', users);
+    })
+    socket.on('removeUser', function(msg){
+        console.log(msg)
+    })
+    socket.on('disconnect', function(){
+        users = users.filter(function(item){
+            return item != socket.user;
+        })
+    })
 })
 http.listen(3000, function(){
     console.log('server start')
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,4 +5,25 @@ $(function(){
     $('.side-bar-toggle').on('click', function(){
         $('.side-bar').toggleClass('open');
     })
+
+
+    var socket = io(), $login = $('.chat-login'), $room = $('.chat-room'), $userList = $('#userList'),
+        $userCount = $('#userCount');
+    $('#loginForm').submit(function(){
+        var $loginName = $('#loginName').val().trim();
+        if(!$loginName){
+            return false;
+        }
+        $login.fadeOut(function(){
+            $room.fadeIn();
+        });
+        socket.emit('addUser', $loginName);
+        return false;
+    })
+    socket.on('queryUser', function(users){
+        $.each(users, function(index, user){
+            $userList.append('<li class="list-group-item">' + user + '</li>')
+        })
+        $userCount.text(users.length);
+    })
 })
